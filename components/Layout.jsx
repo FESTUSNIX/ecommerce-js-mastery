6d2682727434759d621a3f419b38c5f01cdd3173
Festus <matkowsky808@gmail.com>
2023-02-20 00:59:45 +0100

    Init

---
+++ components/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Head from 'next/head'
+import { Navbar, Footer } from '.'
+
+const Layout = ({ children }) => {
+	return (
+		<div className='layout'>
+			<Head>
+				<title>JS Mastery Ecommerce</title>
+			</Head>
+
+			<header>
+				<Navbar />
+			</header>
+
+			<main className='main-container'>{children}</main>
+
+			<footer>
+				<Footer />
+			</footer>
+		</div>
+	)
+}
+
+export default Layout
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'j8o8sii3',
+    dataset: 'production'
+  }
+})
